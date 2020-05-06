1e65c654214d5952cd571e3449b924a8a0f6213f
fad333 <47511104+fad333@users.noreply.github.com>
2020-05-06 14:28:14 +0800

    Update dark.py

--- dark.py
+++ dark.py
@@ -13,7 +13,7 @@ sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
-br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('User-Agent','Mozilla/5.0 (Linux; Android; AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36")]
 
 
 def keluar():
